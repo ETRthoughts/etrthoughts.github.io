597bfe03e135e842dc6b6a521d85cb75b8153d99
ETR Thoughts <etrthoughts@proton.me>
2025-09-19 08:32:28 -0700

    as

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "null",
     },
     locale: "en-US",
-    baseUrl: "/",
+    baseUrl: "etrthoughts.github.io",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
